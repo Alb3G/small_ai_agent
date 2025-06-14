226816e36bd13a5b44425d8f9a206f83ae6e16e4
Alb3G <guzmanmorenoalberto1998@gmail.com>
2025-06-14 13:25:22 +0200

    Refactor: Packed code in utility functions and now the llm can perform operations over the files we specify through the prompt

--- main.py
+++ main.py
@@ -5,52 +5,75 @@ from google import genai
 from google.genai import types
 from constants import SYSTEM_PROMPT
 from functions.genai_schemas import *
+from functions.call_function import call_function
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
+def main():
+	load_dotenv()
+	api_key = os.environ.get("GEMINI_API_KEY")
 
-if len(sys.argv) <= 1:
-	print("Error: No prompt added to the call")
-	sys.exit(1)
+	if len(sys.argv) <= 1:
+		print("Error: No prompt added to the call")
+		sys.exit(1)
 
-user_prompt = sys.argv[1]
-is_verbose = False
+	user_prompt = sys.argv[1]
+	is_verbose = False
 
-if len(sys.argv) == 3 and sys.argv[2] == "--verbose":
-	is_verbose = True
+	if len(sys.argv) == 3 and sys.argv[2] == "--verbose":
+		is_verbose = True
 
-messages = [
-	types.Content(role="user", parts=[types.Part(text=user_prompt)])
-]
+	contents = [
+		types.Content(role="user", parts=[types.Part(text=user_prompt)])
+	]
 
-# Available schemas
-available_functions = types.Tool(
-    function_declarations = [
-        schema_get_files_info,
-		schema_get_file_content,
-		schema_write_file,
-		schema_run_python
-    ]
-)
+	# Available schemas
+	available_functions = types.Tool(
+    	function_declarations = [
+        	schema_get_files_info,
+			schema_get_file_content,
+			schema_write_file,
+			schema_run_python
+    	]
+	)
 
-# Genai
-client = genai.Client(api_key=api_key)
-response = client.models.generate_content(
-	model = 'gemini-2.0-flash-001', 
-	contents = messages,
-	config = types.GenerateContentConfig(
-		system_instruction = SYSTEM_PROMPT,
-		tools = [available_functions]
-	),
-)
+	# Genai
+	client = genai.Client(api_key=api_key)
+	generate_content(client, contents, is_verbose, available_functions)
 
-if response.function_calls:
-	for func in response.function_calls:
-		print(f"Calling function: {func.name}({func.args})")
-else:
-	print(response.text)
+def generate_content(client, contents, is_verbose, available_functions):
+	response = client.models.generate_content(
+		model = 'gemini-2.0-flash-001', 
+		contents = contents,
+		config = types.GenerateContentConfig(
+			system_instruction = SYSTEM_PROMPT,
+			tools = [available_functions]
+		),
+	)
 
-if is_verbose:
-	print(f"User prompt: {user_prompt}")
-	print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-	print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+	if is_verbose:
+		print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+		print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+	if not response.function_calls:
+		return response.text
+	
+	function_responses = []
+
+	for func_call in response.function_calls:
+		function_call_result = call_function(func_call, verbose=is_verbose)
+		
+		if (
+			not function_call_result.parts
+			or not function_call_result.parts[0].function_response
+		):
+			raise Exception("empty function call result")
+		
+		if is_verbose:
+			print(f"-> {function_call_result.parts[0].function_response.response}")
+		
+		function_responses.append(function_call_result.parts[0])
+
+		if not function_responses:
+			raise Exception("no function responses generated, exiting.")
+
+if __name__ == "__main__":
+	main()
